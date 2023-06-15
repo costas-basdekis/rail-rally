ec53b08177c85e1a3055a5389d618ec965791b62
Costas Basdekis <costas.basdekis@gmail.com>
2023-06-15 08:07:21 +0100

    Extract `RGrid`

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,67 +1,27 @@
 import styles from "@/components/Home.module.scss";
-import {About, RTile} from "@/components";
+import {About, RGrid} from "@/components";
 import {Component} from "react";
 import * as rails from "@/rails";
 
 interface HomeState {
   grid: rails.Grid,
-  selectedTile: rails.Tile | null,
-  connectableTiles: rails.Tile[],
 }
 
 export class Home extends Component<object, HomeState>{
   state: HomeState = {
     grid: rails.Grid.fromSize(20, 20),
-    selectedTile: null,
-    connectableTiles: [],
   };
 
   render() {
-    const {grid, selectedTile, connectableTiles} = this.state;
+    const {grid} = this.state;
     return (
       <main className={styles.main}>
         <h1>Rail Rally</h1>
         <svg width={400} height={400} style={{backgroundColor: "white"}}>
-          {Array.from(grid.tiles()).map(tile => (
-            <RTile
-              key={`${tile.x},${tile.y}`}
-              tile={tile}
-              selected={selectedTile === tile}
-              connectable={connectableTiles.includes(tile)}
-              onTileClick={this.onTileClick}
-            />
-          ))}
+          <RGrid grid={grid} />
         </svg>
         <About/>
       </main>
     );
   }
-
-  onTileClick = (tile: rails.Tile) => {
-    this.setState<"selectedTile" | "connectableTiles">(({grid, selectedTile, connectableTiles}) => {
-      if (tile === selectedTile) {
-        return {
-          selectedTile: null,
-          connectableTiles: [],
-        };
-      }
-      if (!selectedTile) {
-        return {
-          selectedTile: tile,
-          connectableTiles: tile.getConnectableTiles(grid),
-        };
-      }
-      if (connectableTiles.includes(tile)) {
-        grid.connect([selectedTile, tile]);
-        return {
-          selectedTile: tile,
-          connectableTiles: tile.getConnectableTiles(grid),
-        };
-      }
-      return {
-        selectedTile: tile,
-        connectableTiles: tile.getConnectableTiles(grid),
-      };
-    });
-  };
 }
---
+++ src/components/RGrid.tsx
@@ -0,0 +1,62 @@
+import {Component} from "react";
+import {RTile} from "@/components/RTile";
+import * as rails from "@/rails";
+
+interface RGridProps {
+  grid: rails.Grid,
+}
+
+interface RGridState {
+  selectedTile: rails.Tile | null,
+  connectableTiles: rails.Tile[],
+}
+
+export class RGrid extends Component<RGridProps, RGridState> {
+  state: RGridState = {
+    selectedTile: null,
+    connectableTiles: [],
+  };
+
+  render() {
+    const {grid} = this.props;
+    const {selectedTile, connectableTiles} = this.state;
+    return Array.from(grid.tiles()).map(tile => (
+      <RTile
+        key={`${tile.x},${tile.y}`}
+        tile={tile}
+        selected={selectedTile === tile}
+        connectable={connectableTiles.includes(tile)}
+        onTileClick={this.onTileClick}
+      />
+    ));
+  }
+
+  onTileClick = (tile: rails.Tile) => {
+    this.setState<"selectedTile" | "connectableTiles">(({selectedTile, connectableTiles}) => {
+      const {grid} = this.props;
+      if (tile === selectedTile) {
+        return {
+          selectedTile: null,
+          connectableTiles: [],
+        };
+      }
+      if (!selectedTile) {
+        return {
+          selectedTile: tile,
+          connectableTiles: tile.getConnectableTiles(grid),
+        };
+      }
+      if (connectableTiles.includes(tile)) {
+        grid.connect([selectedTile, tile]);
+        return {
+          selectedTile: tile,
+          connectableTiles: tile.getConnectableTiles(grid),
+        };
+      }
+      return {
+        selectedTile: tile,
+        connectableTiles: tile.getConnectableTiles(grid),
+      };
+    });
+  };
+}
