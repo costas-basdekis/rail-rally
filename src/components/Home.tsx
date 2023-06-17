db3dabae233f9fb3da4a2917196995bf3a42d97d
Costas Basdekis <costas.basdekis@gmail.com>
2023-06-17 08:39:40 +0100

    Add `New` button

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -20,6 +20,7 @@ export class Home extends Component<object, HomeState>{
       <main className={styles.main}>
         <h1>Rail Rally</h1>
         <RSerialisedGrids grid={grid} onGridUpdate={this.onGridUpdate} />
+        <button onClick={this.onNewGridClick}>New</button>
         <svg width={400} height={400} style={{backgroundColor: "white"}}>
           <RGrid grid={grid} />
           <RTrain grid={grid} train={train} onTrainUpdate={this.onTrainUpdate} />
@@ -35,5 +36,9 @@ export class Home extends Component<object, HomeState>{
 
   onGridUpdate = (grid: rails.Grid) => {
     this.setState<"grid">({grid});
+  };
+
+  onNewGridClick = () => {
+    this.setState<"grid">({grid: rails.Grid.fromSize(20, 20), train: null});
   };
 }
