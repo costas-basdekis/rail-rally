32874bd69a55341885425f13f11c1edc728b679b
Costas Basdekis <costas.basdekis@gmail.com>
2023-06-17 14:13:38 +0100

    More robust connection rendering

--- src/components/RTile.tsx
+++ src/components/RTile.tsx
@@ -30,9 +30,13 @@ class RTileConnections extends Component<{tile: rails.Tile}, {}> {
     return (
       <g transform={`translate(${tile.x * 20}, ${tile.y * 20})`}>
         {tile.internalConnections.map(([first, second]) => {
-          const firstPosition = rails.connectionDirections.positionByDirectionMap.get(first)!;
-          const secondPosition = rails.connectionDirections.positionByDirectionMap.get(second)!;
-          if (rails.connectionDirections.oppositeMap[first] === second) {
+          const [edge, corner] = first.includes("-")
+            ? [second, first] as const
+            : [first, second] as const;
+          const arcConfiguration = rails.connectionDirections.arcMap[edge]?.[corner];
+          if (!arcConfiguration) {
+            const firstPosition = rails.connectionDirections.positionByDirectionMap.get(first)!;
+            const secondPosition = rails.connectionDirections.positionByDirectionMap.get(second)!;
             return (
               <line
                 key={`${first}:${second}`}
@@ -42,10 +46,8 @@ class RTileConnections extends Component<{tile: rails.Tile}, {}> {
               />
             );
           }
-          const [edge, corner, edgePosition] = first.includes("-")
-            ? [second, first, secondPosition] as const
-            : [first, second, firstPosition] as const;
-          const {angle, sweep, offsetX, offsetY} = rails.connectionDirections.arcMap[edge][corner];
+          const {angle, sweep, offsetX, offsetY} = arcConfiguration;
+          const edgePosition = rails.connectionDirections.positionByDirectionMap.get(edge)!
           const endX = (edgePosition.x + offsetX) * 20 + 20 * 1.5 * Math.cos(angle);
           const endY = (edgePosition.y + offsetY) * 20 + 20 * 1.5 * Math.sin(angle);
           return (
